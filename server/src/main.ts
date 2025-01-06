85ce7cdd0e25a2dfd522653b3d34235ff60cda7e
Charles Catto <charlescatto@outlook.fr>
2025-01-06 16:26:23 +0100

    commit serv et main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,26 @@
+// // Load environment variables from .env file
+// import "dotenv/config";
+
+// // Check database connection
+// // Note: This is optional and can be removed if the database connection
+// // is not required when starting the application
+// import "../database/checkConnection";
+
+// // Import the Express application from ./app
+// import app from "./app";
+
+// // Get the port from the environment variables
+// const port = process.env.APP_PORT;
+
+// // Start the server and listen on the specified port
+// app
+//   .listen(port, () => {
+//     console.info(`Server is listening on port ${port}`);
+//   })
+//   .on("error", (err: Error) => {
+//     console.error("Error:", err.message);
+//   });
+
 // Load environment variables from .env file
 import "dotenv/config";
 
@@ -8,6 +31,11 @@ import "../database/checkConnection";
 
 // Import the Express application from ./app
 import app from "./app";
+
+// Add a new GET / route
+app.get("/", (req, res) => {
+  res.send("Welcome to Wild Series!");
+});
 
 // Get the port from the environment variables
 const port = process.env.APP_PORT;
